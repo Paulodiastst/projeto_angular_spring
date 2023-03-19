3e1064081157f4690601aa66d931a866f6eb4f01
Paulo Roberto <paulodiastst@hotmail.com>
2023-03-19 20:39:21 -0300

    Criando rota de edição

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -3,10 +3,7 @@ package br.com.projeto.api.controle;
 import br.com.projeto.api.modelo.Cliente;
 import br.com.projeto.api.repositorio.Repositorio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controle {
@@ -22,6 +19,11 @@ public class Controle {
     @GetMapping("/")
     public Iterable<Cliente> selecionar(){
         return acao.findAll();
+    }
+
+    @PutMapping("/")
+    public Cliente editar(@RequestBody Cliente c){
+        return acao.save(c);
     }
 
 }
